3efb0d135f688de6b849de9d639bebf063376b87
Brent Cook <busterb@gmail.com>
2015-03-27 23:53:24 -0500

    remove unused stub functions, init code

--- include/tls.h
+++ include/tls.h
@@ -5,27 +5,4 @@
 
 #ifdef HAVE_LIBTLS
 #include_next <tls.h>
-
-#else
-
-#ifndef LIBCOMPAT_LIBTLS_H
-#define LIBCOMPAT_LIBTLS_H
-
-#include <sys/types.h>
-#include <stdint.h>
-
-static inline int
-tls_init(void)
-{
-	return -1;
-}
-
-static inline uint8_t *
-tls_load_file(const char *_file, size_t *_len, char *_password)
-{
-	return NULL;
-}
-
-#endif
-
 #endif
