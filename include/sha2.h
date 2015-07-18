7ec206890669076f09bf84f1572f32b351f79391
Brent Cook <bcook@openbsd.org>
2015-07-18 17:31:32 -0500

    remove unnecessary header defines, condense md5

--- include/sha2.h
+++ include/sha2.h
@@ -3,25 +3,9 @@
  * sha2.h compatibility shim
  */
 
-#ifdef HAVE_SHA2_H
-#include_next <sha2.h>
-#else
-
 #include "sha2_openbsd.h"
-
-#define __weak_alias(alias,sym)
-
-#define SHA224Transform(a, b) SHA256Transform(a, b)
-#define SHA224Update(a, b, c) SHA256Update(a, b, c)
-#define SHA224Pad(a) SHA256Pad(a)
-
-#define SHA384Transform(a, b) SHA512Transform(a, b)
-#define SHA384Update(a, b, c) SHA512Update(a, b, c)
-#define SHA384Pad(a) SHA512Pad(a)
 
 #define SHA512_CTX SHA2_CTX
 #define SHA512_Init(ctx) SHA512Init(ctx)
 #define SHA512_Update(ctx, buf, len) SHA512Update(ctx, (void *)buf, len)
 #define SHA512_Final(digest, ctx) SHA512Final(digest, ctx)
-
-#endif
