f100b7bdd1649bc752038a763c81fc3d2e50c737
Brent Cook <bcook@rapid7.com>
2015-07-17 23:59:09 -0600

    undef MODEMASK from Solaris 10

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -11,4 +11,12 @@
 	 ((struct sockaddr*)(X))->sa_family == AF_INET6 ? sizeof(struct sockaddr_in6) : \
 	 ((struct sockaddr*)(X))->sa_family == AF_UNSPEC ? sizeof(struct sockaddr) : \
 	   sizeof(struct sockaddr))
+
+/*
+ * Prevent Solaris 10 system header leakage
+ */
+#ifdef MODEMASK
+#undef MODEMASK
+#endif
+
 #endif
