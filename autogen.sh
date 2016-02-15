5b637000b30868dc6b5c751a3343b2251848ff78
Brent Cook <bcook@openbsd.org>
2016-02-15 11:44:28 -0600

    grab newer versions of gnu config scripts

--- autogen.sh
+++ autogen.sh
@@ -5,3 +5,6 @@ set -e
 mkdir -p m4
 rm -fr autom4te.cache
 autoreconf -i
+chmod -R u+w .
+curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' > config.guess
+curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' > config.sub
