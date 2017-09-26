17aa5d98191c4dc85d0645443fc97a12601ae8da
Baruch Siach <baruch@tkos.co.il>
2017-09-26 14:55:31 +0300

    Revert "use __dead macro from sys/cdefs.h"
    This partially reverts commit a7577667d673f49800e15461fe384817a1572eae.
    
    The musl C library does not use an internal cdefs.h header. As a result,
    the definitions in sys/cdefs.h have no effect. This breads the build as
    follows:
    
    In file included from adjfreq_linux.c:23:0:
    ../src/ntpd.h:448:8: error: expected ‘=’, ‘,’, ‘;’, ‘asm’ or ‘__attribute__’ before ‘void’
     __dead void fatal(const char *, ...)
            ^~~~
    ../src/ntpd.h:450:8: error: expected ‘=’, ‘,’, ‘;’, ‘asm’ or ‘__attribute__’ before ‘void’
     __dead void fatalx(const char *, ...)
            ^~~~
    
    Refer the related musl FAQ about cdefs.h:
    
      http://wiki.musl-libc.org/wiki/FAQ#Q:_I.27m_trying_to_compile_something_against_musl_and_I_get_error_messages_about_sys.2Fcdefs.h
    
    Keep the compiler attribute definition.
    
    Remove also from noinst_HEADERS.

--- include/sys/types.h
+++ include/sys/types.h
@@ -14,6 +14,10 @@
 #include <_bsd_types.h>
 #endif
 
+#if !defined(HAVE_ATTRIBUTE__DEAD) && !defined(__dead)
+#define __dead		__attribute__((__noreturn__))
+#endif
+
 #if !defined(HAVE_ATTRIBUTE__BOUNDED__) && !defined(__bounded__)
 # define __bounded__(x, y, z)
 #endif
