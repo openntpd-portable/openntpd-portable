38d43a876149abe64a9b10dda089db3b9744ebcb
Brent Cook <busterb@gmail.com>
2026-09-17 10:50:50 -0500

    resync with upstream (quick update, only tested on OpenBSD)

--- update.sh
+++ update.sh
@@ -52,7 +52,7 @@ ${CP_LIBC} "${libc_src}/crypt/arc4random.c" compat
 ${CP_LIBC} "${libc_src}/crypt/arc4random_uniform.c" compat
 ${CP_LIBC} "${libc_src}/crypt/chacha_private.h" compat
 ${CP_LIBC} "${libc_src}/hash/md5.c" compat
-${CP_LIBC} "${libc_src}/hash/sha2.c" compat
+${CP_LIBC} "${libc_src}/hash/sha256.c" compat
 for i in "${arc4random_src}"/getentropy_*.c; do
 	sed -e 's/openssl\/sha.h/sha2.h/' < "${i}" > compat/`basename "${i}"`
 done
